b7f4260ad11607a0fe51676bd7fb48266c1eb24f
alex2two <161016344+alex2two@users.noreply.github.com>
2025-03-20 09:56:22 +0100

    2

--- my_append.pl
+++ my_append.pl
@@ -9,3 +9,7 @@ sera cierto si ListaResulado unifica con los elementos de Lista1 y, a continuaci
 my_append([],List2,List2).
 
 my_append([Cab1|Resto1],List2, [Cab1|R]):- my_append(Resto1, List2, R).
+
+/*
+my_append(List1,[Cab1|Resto1], [R|Resto1]):- my_append(List1, Cab1, R).
+*/
